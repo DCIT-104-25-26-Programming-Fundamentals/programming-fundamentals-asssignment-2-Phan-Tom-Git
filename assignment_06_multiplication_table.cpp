a8cd9b40f122608b26f5673ee97ce3d219e24caa
Oumar Ndongo <74785844+Phan-Tom-Git@users.noreply.github.com>
2026-08-02 20:46:36 +0000

    Update assignment_06_multiplication_table.cpp

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,32 @@
 #include <iostream>
 using namespace std;
 
+void printTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printTable(number);
+
+    int n;
+    cout << "Enter a number N: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        if (num != n) cout << "---" << endl;
+    }
+
+    return 0;
+}
